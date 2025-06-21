61ffef2af16b79f4adbaf5b91dad37a393b8b0be
Utkarsh067 <utkarshjn02@gmail.com>
2025-06-21 13:33:40 +0000

    Final commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD!"
+    return "Hello, I am Utkarsh Jain, I made flask app, dockerised it and implemented CI/CD using GitHub Actions and deploying locally with Minikube"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
